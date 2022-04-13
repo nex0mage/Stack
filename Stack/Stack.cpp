26060edf159b4d1f1bc3bfcf4c7958851e42bb86
trace <trace@DESKTOP-TC3U6ED>
2022-04-13 20:47:00 +0300

    Добавьте файлы проекта.

---
+++ Stack/Stack.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <stack>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a = 0;
+	int b = 0;
+	ifstream read;
+	ofstream out;
+	read.open("c:\\Users\\trace\\source\\repos\\Stack\\Stackread.txt");
+	out.open("c:\\Users\\trace\\source\\repos\\Stack\\Out.txt");
+
+	if (!read)
+	{
+		cout << "Файл не открыт!" << endl;
+	}
+	else
+	{
+		cout << "Файл открыт!" << endl;
+	}
+
+	stack <int> stack;
+	while (!read.eof())
+	{
+		read >> a;
+		cout << a << " ";
+		stack.push(a);
+	}
+	cout << endl;;
+	cout << "1 - нечетн, 2 - четн: ";
+	cin >> b;
+	
+	switch (b) 
+	{
+	case 1:
+		while (stack.empty() == false)
+		{
+			if (stack.top() % 2 == 0)
+			{
+				stack.pop();
+
+			}
+			else if (stack.top() % 2 != 0)
+			{
+				a = stack.top();
+				cout << a << " ";
+				stack.pop();
+				out << a << " ";
+			}
+		}
+		break;
+	case 2:
+		while (stack.empty() == false)
+		{
+			if (stack.top() % 2 != 0)
+			{
+				stack.pop();
+
+			}
+			else if (stack.top() % 2 == 0)
+			{
+				a = stack.top();
+				cout << a << " ";
+				stack.pop();
+				out << a << " ";
+			}
+		}
+		break;
+	}
+
+}
